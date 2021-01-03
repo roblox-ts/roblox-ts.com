bde731e0f4cace2e1952034e0dbd7041e354d86b
osyrisrblx <osyrisrblx@gmail.com>
2021-01-02 20:43:07 -0800

    Use BrowserOnly for playground

--- src/pages/playground/index.tsx
+++ src/pages/playground/index.tsx
@@ -1,13 +1,16 @@
+import BrowserOnly from "@docusaurus/BrowserOnly";
 import Layout from "@theme/Layout";
 import React from "react";
 import Playground from "../../components/Playground";
 
 export default () => {
 	return (
-		<div style={{ overflow: "hidden" }}>
-			<Layout noFooter>
-				<Playground />
-			</Layout>
-		</div>
+		<BrowserOnly>
+			<div style={{ overflow: "hidden" }}>
+				<Layout noFooter>
+					<Playground />
+				</Layout>
+			</div>
+		</BrowserOnly>
 	);
 };
