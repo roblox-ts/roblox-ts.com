cc12b6b25545dd0451f73b3ea3c35ea52c1e7c03
osyrisrblx <osyrisrblx@gmail.com>
2021-12-29 19:03:09 -0800

    Add "Syncing with Rojo" guide

---
+++ docs/components/downArrow.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const DownArrow = () => (
+	<div
+		style={{
+			width: 100,
+			fontSize: "32px",
+			marginTop: "-20px",
+		}}
+	>
+		<center>⬇️</center>
+	</div>
+);
+
+export default DownArrow;
