5ea1730d056e077f4b70f7e8529d08a8429576ba
osyrisrblx <osyrisrblx@gmail.com>
2022-07-13 12:30:49 -0400

    Fix favicon reference

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -5,7 +5,7 @@ module.exports = {
 	baseUrl: "/",
 	onBrokenLinks: "throw",
 	onBrokenMarkdownLinks: "warn",
-	favicon: "img/favicon.ico",
+	favicon: "img/roblox-ts.svg",
 	organizationName: "roblox-ts",
 	projectName: "roblox-ts",
 	plugins: ["docusaurus-node-polyfills"],
