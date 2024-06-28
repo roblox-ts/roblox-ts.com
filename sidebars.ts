cb4652ebb4a31c3c163cbfdbd80a0b10cdf49b41
osyrisrblx <osyrisrblx@gmail.com>
2024-06-28 17:53:48 -0400

    faq.mdx -> faq/index.mdx

--- sidebars.ts
+++ sidebars.ts
@@ -1,39 +1,34 @@
 import type { SidebarsConfig } from "@docusaurus/plugin-content-docs";
 
 const sidebars: SidebarsConfig = {
-  docs: [
-    "introduction",
-    "quick-start",
-    "usage",
-    "setup-guide",
-    "faq",
-    {
-      type: "category",
-      label: "API",
-      items: [
-        "api/roblox-api",
-        "api/constructors",
-        "api/functions",
-        "api/utility-types",
-      ],
-    },
-    {
-      type: "category",
-      label: "Guides",
-      items: [
-        "guides/syncing-with-rojo",
-        "guides/callbacks-vs-methods",
-        "guides/datatype-math",
-        "guides/indexing-children",
-        "guides/lua-tuple",
-        "guides/roact-jsx",
-        "guides/using-existing-luau",
-        "guides/luau-packages",
-        "guides/typescript-packages",
-        "guides/typescript-transformers",
-      ],
-    },
-  ],
+	docs: [
+		"introduction",
+		"quick-start",
+		"usage",
+		"setup-guide",
+		"faq/index",
+		{
+			type: "category",
+			label: "API",
+			items: ["api/roblox-api", "api/constructors", "api/functions", "api/utility-types"],
+		},
+		{
+			type: "category",
+			label: "Guides",
+			items: [
+				"guides/syncing-with-rojo",
+				"guides/callbacks-vs-methods",
+				"guides/datatype-math",
+				"guides/indexing-children",
+				"guides/lua-tuple",
+				"guides/roact-jsx",
+				"guides/using-existing-luau",
+				"guides/luau-packages",
+				"guides/typescript-packages",
+				"guides/typescript-transformers",
+			],
+		},
+	],
 };
 
 export default sidebars;
